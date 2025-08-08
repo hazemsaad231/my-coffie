0d27f4149da3ec2b1b2845d3c41924fad3f79156
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-08-08 18:47:19 +0300

    goo

--- src/app/components/feedback/page.tsx
+++ src/app/components/feedback/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+
 const FedbackPage = () => {
   return (
     <div className='text-center py-10'style={{ fontFamily: 'Geist_Mono' }}>
@@ -17,7 +18,7 @@ const FedbackPage = () => {
     </div>
     <div className='text-8xl font-bold text-amber-900 relative right-60'>‘‘</div>
         <p className="text-gray-600 w-full m-auto mt-2 text-lg p-2">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+         Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset.....</p>
              <div className='flex flex-col items-center justify-center mt-4'>
                 <h1 className="text-2xl sm:text-2xl md:text-3xl lg:text-3xl font-bold text-amber-900">Jonny Thomas</h1>
