4ea5261b3ff4d2c133379991054a8619aaef99f1
hazemsaad231 <166450789+hazemsaad231@users.noreply.github.com>
2025-08-09 12:37:52 +0300

    push

--- src/app/components/footer/page.tsx
+++ src/app/components/footer/page.tsx
@@ -71,7 +71,7 @@ const FooterPage = () => {
                     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 md:place-items-center gap-10 p-10'>
                         <div className='text-white'>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                 Lorem Ipsum has been the industrys standard dummy text ever since the 1500s,
                                  when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                             <ul></ul>
                         </div>
